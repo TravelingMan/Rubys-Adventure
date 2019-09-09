7398647b94be7733344355c4bb35f49461866763
S. Hall <shall.usnavy@gmail.com>
2019-09-09 19:21:59 -0400

    Add license info and dedication to Splash

--- Assets/Scripts/SplashScreen.cs
+++ Assets/Scripts/SplashScreen.cs
@@ -3,12 +3,16 @@ using UnityEngine.SceneManagement;
 
 public class SplashScreen : MonoBehaviour
 {
-    [SerializeField] private float transitionTime = 2.0f;
+    [SerializeField] private float transitionTime = 6.0f;
     private float _elapsed = 0f;
     
     void Update()
     {
-        if (_elapsed < transitionTime)
+        if (Input.anyKeyDown)
+        {
+            SceneManager.LoadScene(1);
+        }
+        else if (_elapsed < transitionTime)
         {
             _elapsed += Time.deltaTime;
         }
