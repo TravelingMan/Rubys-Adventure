1cfb11c2d05ed426eaf26fa85fe59b0989ca1a81
S. Hall <shall.usnavy@gmail.com>
2019-09-09 19:05:59 -0400

    Add splash screen and transition time

---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+    [SerializeField] private float transitionTime = 2.0f;
+    private float _elapsed = 0f;
+    
+    void Update()
+    {
+        if (_elapsed < transitionTime)
+        {
+            _elapsed += Time.deltaTime;
+        }
+        else
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
